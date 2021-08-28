413e53bc161158d72ce1d941cfff7618d3fdb127
duongwolf2510 <84836850+duongwolf2510@users.noreply.github.com>
2021-08-28 21:48:25 +0700

    Update effect.js

--- effect.js
+++ effect.js
@@ -101,13 +101,6 @@ $('document').ready(function(){
 			loopSeven();
 		});
 	}
-	function loopSeven() {
-		var randleft = 1000*Math.random();
-		var randtop = 500*Math.random();
-		$('#b7').animate({left:randleft,bottom:randtop},10000,function(){
-			loopSeven();
-		});
-	}
 	function loopEight() {
 		var randleft = 1000*Math.random();
 		var randtop = 500*Math.random();
